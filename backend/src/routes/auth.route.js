3ca69a6fc89a7a7332dc67b1116c1878fa48cf59
GhugareSanika <144719999+GhugareSanika@users.noreply.github.com>
2025-07-08 17:55:09 +0530

    Authentication Done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,19 @@
 import express from "express";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
 
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup Route");
-});
-
-router.get("/login", (req, res) => {
-  res.send("Signup Route");
-});
-
-router.get("/logout", (req, res) => {
-  res.send("Signup Route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
